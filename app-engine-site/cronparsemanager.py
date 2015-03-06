1728d878171208064a950fb933e776dcbe27d3a8
Nikita Pestrov <ipestrov@gmail.com>
2015-03-06 14:29:53 +0300

    Parsing and parser for cron

---
+++ app-engine-site/cronparsemanager.py
@@ -0,0 +1,22 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+import geotwitter
+import logging
+
+class ParseHandler(webapp.RequestHandler):
+  def get(self):
+      self.response.write("HELLO GET PRE")
+      geotwitter.main()
+      self.response.write("HELLO GET")
+  def post(self):
+      self.response.write("HELLO POST PRE")
+      geotwitter.main()
+      self.response.write("HELLO POST")
+
+logging.error('Before')
+application = webapp.WSGIApplication([('/parsing', ParseHandler)],
+                                     debug=True)
+logging.error('After')
+
+if __name__ == '__main__':
+  run_wsgi_app(application)
